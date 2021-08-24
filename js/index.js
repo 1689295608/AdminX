75072f024259b67c853ac6761e579ed272f30330
喵丶可爱 <50663849+1689295608@users.noreply.github.com>
2021-08-24 15:33:08 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ function SetClickSelecte(elements) {
                     selecte(elements[i]);
                 } else {
                     if (elements[i].tagName == "FILE") {
-                        window.open(`?operstion=edit&dir=${encodeURIComponent(path)}$file=${encodeURIComponent(files[i].innerText)}`);
+                        window.open(`?operstion=edit&dir=${encodeURIComponent(path)}&file=${encodeURIComponent(files[i].innerText)}`);
                     } else {
                         window.location.search = `?dir=${encodeURIComponent(path)}`;
                     }
